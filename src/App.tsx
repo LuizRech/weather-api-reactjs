231de38b2f29d24bae33e67269762b5aee147de2
Luiz Rech <luizadre@hotmail.com>
2021-11-22 16:47:08 -0300

    Search Bar Location

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 import Connection from './config/Connection';
 
-
-  fetch(Connection()).then(response => response.json())
-    .then(data => {console.log(data)});
-
-
-
 function App() {
+
+  const [temp_c, setTemp_c] = useState();
+  const [locationName, setLocationName] = useState();
+  const [keywords, setKeywords] = useState('')
+
+  useEffect(() => {
+
+    fetch(Connection('Dublin')).then(response => response.json())
+    .then(data => {
+      console.log(data);
+  
+      setTemp_c(data.current.temp_c);
+      setLocationName(data.location.name);
+    });
+
+  }, []);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    
+    fetch(Connection(keywords)).then(response => response.json())
+    .then(data => {
+      console.log(data);
+  
+      setTemp_c(data.current.temp_c);
+      setLocationName(data.location.name);
+    });
+
+  }
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
+        <form onSubmit={handleSubmit}>
+
+          <input type="text" 
+            placeholder="Search Location" 
+            name="searchLocation" 
+            id="searchLocation" 
+            className="searchLocationBar" 
+            value={keywords}
+            onChange={e => setKeywords(e.target.value)}
+          />
+        
+        </form>
+
         <p>
-          
+          <h1>Location: {locationName}</h1>
+          <h2>Temp: {temp_c}</h2>
         </p>
       </header>
     </div>
--- src/config/Connection.tsx
+++ src/config/Connection.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 
-const Connection = () => {
+const Connection = (Loc: String) => {
   const urlApi = 'https://api.weatherapi.com/v1/';
   const typeReq = 'forecast.json';
   const key = '?key='; //YOUR_KEY
-  const location = '&q=Dublin';
+  let location = '&q=Dublin';  
+ 
+  if(Loc) {
+    location = `&q=${Loc}`;
+  }
+  
   const days = null; //should have the days s
 
   const ret = urlApi+typeReq+key+location;
-
+  
   return ret;
 }
 
